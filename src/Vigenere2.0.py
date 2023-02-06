5899343a0b62d905cd6389f31810fab3ae11a0fc
hookahtagen <hendrik.siemens@my.com>
2023-02-06 22:10:56 +0100

    Update
    + Updated Vigenere2.0.py
       + extracted some strings to dictionaries
          for more readability of the 'core' code.

--- src/Vigenere2.0.py
+++ src/Vigenere2.0.py
@@ -59,12 +59,19 @@ def poly_vigenere_decrypt(ciphertext, key, alphabets):
     return plaintext
 
 def main( alpha_set: str, mode: str, message: str, key: str, log: Logger ) -> None:
+    main_string_dict = {
+        'save_file': '../data/alphabets.txt',
+        'log_success': f'Successfully loaded custom alphabet set from {alpha_file}',
+        'invalid_alpha_set': f'Invalid alphabet set: {alpha_set} (must be \'d\' or \'c\')',
+        'entered_message': f'Entered message: {message}',
+        'invalid_mode': f'Invalid mode: {mode} (must be \'e\' or \'d\')'
+    }
     
     if alpha_set == 'd':
         abc = Alphabet( key )
         alphabets = abc.polyalphabet
     elif alpha_set == 'c':
-        alpha_file = '../data/alphabets.txt'
+        alpha_file = main_string_dict['save_file'] # string replaced for readability
         alphabets: dict[str, str] = { }
         
         with open( alpha_file, 'r') as inp:
@@ -73,29 +80,39 @@ def main( alpha_set: str, mode: str, message: str, key: str, log: Logger ) -> No
                 key, value = line.split( ':' )
                 alphabets[ key ] = value
                 
-        log.info( f'Successfully loaded custom alphabet set from {alpha_file}' )
+        log.info( main_string_dict['log_success'] ) # string replaced for readability
     else:
-        log.warning( f'Invalid alphabet set: {alpha_set} (must be \'d\' or \'c\')' )
+        log.warning( main_string_dict['invalid_alpha_set'] ) # string replaced for readability
         exit( 1 )
     
     if mode == 'e':
-        print(f'Entered message: {message}')
+        print( main_string_dict['entered_message'] )  # string replaced for readability
         print("Encrypted message: ", poly_vigenere_encrypt(message, key, alphabets))
     elif mode == 'd':
-        print(f'Entered message: {message}')
+        print( main_string_dict['entered_message'] ) # string replaced for readability
         print("Decrypted message: ", poly_vigenere_decrypt(message, key, alphabets))
     else:
-        print(f'Invalid mode: {mode} (must be \'e\' or \'d\'')
-    pass
+        print( main_string_dict['invalid_mode'] ) # string replaced for readability
+        exit( 1 )
+    
+    return 0
 
 if __name__ == '__main__':
     log = Logger( )
+    menu_text = {
+        'alpha_set': 'Do you wanna use the default alphabet set or use a custom one? (d/c) > ',
+        'mode': 'Enter \'e\' for encrypt or \'d\' for decrypt: ',
+        'message': 'Enter message: ',
+        'example_key': 'Example key: \'ABCFTGJ\'',
+        'key': 'Enter key: '
+    }
     
-    alpha_set = input( "Do you wanna use the default alphabet set or use a custom one? (d/c) > " ).lower()
-    mode = input("Enter \'e' for encrypt or \'d' for decrypt: ").lower()
-    message = input("Enter message: ").upper()
-    print( 'Example key: \'ABCFTGJ\'')
-    key = getpass.getpass("Enter key: ").upper()
+    alpha_set = input( menu_text['alpha_set'] ).lower()
+    mode = input( menu_text['mode'] ).lower()
+    message = input( menu_text['message'] ).upper()
+    print( menu_text['example_key'] )
+    key = getpass.getpass( menu_text['key'] ).upper()
     
-    main(alpha_set, mode, message, key, log)
+    ret = main(alpha_set, mode, message, key, log)
+    exit( ret )
     
