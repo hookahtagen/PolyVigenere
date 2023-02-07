50feb7561406f007261b03592e5da60800543c15
hookahtagen <hendrik.siemens@my.com>
2023-02-07 12:50:07 +0100

    Update
    + You can now easily encrypt and decrypt txt files.
    + Now you can import the Machine class of Vigenere2.0.py
       into other projects. See the documentation for further details

--- src/Vigenere2.0.py
+++ src/Vigenere2.0.py
@@ -1,4 +1,5 @@
 import getpass
+import json
 import logging
 from polyAlpha import Alphabet
 
@@ -33,134 +34,134 @@ class Logger:
 
     def critical( self, message: str ):
         self.logger.critical( message )
-
-def poly_vigenere_encrypt(plaintext: str, key: str, alphabets: dict[str, str] ) -> str:
-    '''
-    Description:
-        This function takes a plaintext string and a key string and encrypts the plaintext using the key.
-        Every character in the plaintext is encrypted using the character in the key that corresponds to it.
-
-    Args:
-        plaintext (str): _description_
-        key (str): _description_
-        alphabets (dict[str, str]): _description_
-
-    Returns:
-        str: _description_
-    '''    
-    
-    ciphertext = ""
-    key_index = 0
-    for char in plaintext:
-        if char.upper() in alphabets[key[key_index % len(key)]]:
-            key_char = key[key_index % len(key)]
-            char_index = alphabets[key_char].find(char.upper())
-            ciphertext += alphabets[key_char][(char_index + 1) % len(alphabets[key_char])]
-        else:
-            ciphertext += char
-        key_index += 1
-    return ciphertext
-
-def poly_vigenere_decrypt(ciphertext: str, key: str, alphabets: dict[str, str]) -> str:
-    '''
-    Description:
-        This function takes a ciphertext string and a key string and decrypts the ciphertext using the key.
         
-    Args:
-        ciphertext (str): _description_
-        key (str): _description_
-        alphabets (dict[str, str]): _description_
-
-    Returns:
-        str: _description_
-    '''    
-    
-    plaintext = ""
-    key_index = 0
-    for char in ciphertext:
-        char_index = alphabets[key[key_index % len(key)]].find(char)
-        if char_index != -1:
-            plaintext += alphabets[key[key_index % len(key)]][alphabets[key[key_index % len(key)]].find(char) - 1]
-        else:
-            plaintext += char
-        key_index += 1
-    return plaintext
-
-def main( alpha_set: str, mode: str, message: str, key: str, log: Logger ) -> None:
-    '''
-    Description:
-        This is the main function of the program. It takes the user input and calls the 
-        appropriate functions to encrypt or decrypt the message.    
-
-    Args:
-        alpha_set (str): _description_
-        mode (str): _description_
-        message (str): _description_
-        key (str): _description_
-        log (Logger): _description_
-
-    Returns:
-        _type_: _description_
-    '''   
-    
-    ret = 1 
-    
-    main_string_dict = {
-        'save_file': '../data/alphabets.txt',
-        'log_success': f'Successfully loaded custom alphabet set from {alpha_file}',
-        'invalid_alpha_set': f'Invalid alphabet set: {alpha_set} (must be \'d\' or \'c\')',
-        'entered_message': f'Entered message: {message}',
-        'invalid_mode': f'Invalid mode: {mode} (must be \'e\' or \'d\')'
-    }
-    
-    if alpha_set == 'd':
-        abc = Alphabet( key )
-        alphabets = abc.polyalphabet
-    elif alpha_set == 'c':
-        alpha_file = main_string_dict['save_file'] # string replaced for readability
-        alphabets: dict[str, str] = { }
+class Machine:
+    def __init__( self ) -> None:
+        self.initialized = "Machine Initialized"
+        return None
         
-        with open( alpha_file, 'r') as inp:
-            for line in inp:
+    def process_message( self, mode: str, key: str, message: str, file_enc: bool, file_name: str ) -> str:
+        self.mode = mode
+        self.key = key
+        self.message = message
+        self.alphabets: dict[ str, str ] = { }
+        self.file_enc = file_enc
+        
+        self.alpha = Alphabet( key )
+        self.alphabets = self.alpha.polyalphabet
+        
+        if self.mode == 'e':
+            self.processed_message = self.poly_vigenere_encrypt( self.message, self.key, self.alphabets )
+        elif self.mode == 'd':
+            self.processed_message = self.poly_vigenere_decrypt( self.message, self.key, self.alphabets )
+        
+        if not self.file_enc:
+            return self.processed_message
+        else:
+            with open( file_name.replace('.txt','_enc.txt'), 'w' ) as file:
+                file.write( self.processed_message )
+            
+            if self.mode == 'd':
+                return "File decrypted successfully"
+            else:
+                return "File encrypted successfully"
+        
+    def poly_vigenere_encrypt(self, plaintext: str, key: str, alphabets: dict[str, str] ) -> str:
+        '''
+        Description:
+            This function takes a plaintext string and a key string and encrypts the plaintext using the key.
+            Every character in the plaintext is encrypted using the character in the key that corresponds to it.
+
+        Args:
+            plaintext (str): _description_
+            key (str): _description_
+            alphabets (dict[str, str]): _description_
+
+        Returns:
+            str: _description_
+        '''    
+        
+        ciphertext = ""
+        key_index = 0
+        for char in plaintext:
+            if char.upper() in alphabets[key[key_index % len(key)]]:
+                key_char = key[key_index % len(key)]
+                char_index = alphabets[key_char].find(char.upper())
+                ciphertext += alphabets[key_char][(char_index + 1) % len(alphabets[key_char])]
+            else:
+                ciphertext += char
+            key_index += 1
+        return ciphertext
+
+    def poly_vigenere_decrypt(self, ciphertext: str, key: str, alphabets: dict[str, str]) -> str:
+        '''
+        Description:
+            This function takes a ciphertext string and a key string and decrypts the ciphertext using the key.
+            
+        Args:
+            ciphertext (str): _description_
+            key (str): _description_
+            alphabets (dict[str, str]): _description_
+
+        Returns:
+            str: _description_
+        '''    
+        
+        plaintext = ""
+        key_index = 0
+        for char in ciphertext:
+            char_index = alphabets[key[key_index % len(key)]].find(char)
+            if char_index != -1:
+                plaintext += alphabets[key[key_index % len(key)]][alphabets[key[key_index % len(key)]].find(char) - 1]
+            else:
+                plaintext += char
+            key_index += 1
+        return plaintext
+
+def main( log: Logger ) -> None:     
+    ret = 1
+    machine = Machine( )
+    log.info( machine.initialized )
+    
+    print( "Welcome to the PolyVigenere Machine" )
+    print( "You have either the option to use the settings file or enter the settings manually")
+    settings_file: bool = True if input( "Do you want to use the settings file? (y/n): ") == 'y' else False
+    
+    file_enc: bool = True if input( "Do you want to encrypt or decrypt a file? (y/n): ") == 'y' else False
+    mode = input( "Do you want to encrypt or decrypt a message? (e/d): " )
+    if settings_file:
+        in_settings: dict[ str, str ] = { }
+        with open( '../data/settings.cfg', 'r' ) as in_file:
+            for line in in_file:
                 line = line.strip()
-                key, value = line.split( ':' )
-                alphabets[ key ] = value
-                
-        log.info( main_string_dict['log_success'] ) # string replaced for readability
-    else:
-        log.warning( main_string_dict['invalid_alpha_set'] ) # string replaced for readability
-        exit( 1 )
-    
-    if mode == 'e':
-        print( main_string_dict['entered_message'] )  # string replaced for readability
-        print("Encrypted message: ", poly_vigenere_encrypt(message, key, alphabets))
-        ret = 0
-    elif mode == 'd':
-        print( main_string_dict['entered_message'] ) # string replaced for readability
-        print("Decrypted message: ", poly_vigenere_decrypt(message, key, alphabets))
+                a_key, value = line.split( '=' )
+                in_settings[ a_key ] = value
+        key = in_settings[ 'key' ].upper( )
+            
+    elif not settings_file:
+        key = input( "Enter the key: " ).upper( )
+        
+    if not file_enc:
+        message = input( "Enter the message: " )
+        p_message = machine.process_message( mode, key, message, file_enc, None )
+        log.info( f'Processed Message: {p_message}')
+        
         ret = 0
     else:
-        print( main_string_dict['invalid_mode'] ) # string replaced for readability
-        exit( 1 )
-    
+        file_name = input( "Enter the file name: " )
+        with open( file_name, 'r' ) as file:
+            message = file.read( )
+        p_message = machine.process_message( mode, key, message, file_enc, file_name )
+        
+        log.info( p_message )
+
+        ret = 1 
+
     return ret
 
 if __name__ == '__main__':
-    log = Logger( )
-    menu_text = {
-        'alpha_set': 'Do you wanna use the default alphabet set or use a custom one? (d/c) > ',
-        'mode': 'Enter \'e\' for encrypt or \'d\' for decrypt: ',
-        'message': 'Enter message: ',
-        'example_key': 'Example key: \'ABCFTGJ\'',
-        'key': 'Enter key: '
-    }
+    log = Logger( )    
+    ret = main( log )
     
-    alpha_set = input( menu_text['alpha_set'] ).lower()
-    mode = input( menu_text['mode'] ).lower()
-    message = input( menu_text['message'] ).upper()
-    print( menu_text['example_key'] )
-    key = getpass.getpass( menu_text['key'] ).upper()
-    
-    ret = main(alpha_set, mode, message, key, log)
     exit( ret )
     
